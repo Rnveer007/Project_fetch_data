31481576214d030faff2d04b7214725026bc4321
Rnveer007 <rvs523474@gmail.com>
2025-01-29 14:07:57 +0530

    updated

--- data.js
+++ data.js
@@ -1,36 +1,54 @@
 import { pokemon } from "./script.js";
 const pokemonsShowBox = document.querySelector(".showPokemons");
+const typeFilter = document.querySelector("#typeFilter"); // Select dropdown
 
 async function fetchPokemonData(url) {
     try {
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
-        return data.sprites.other.dream_world.front_default;
-
-        
-
+        const response = await fetch(url);
+        const data = await response.json();
+        return {
+            image: data.sprites.other.dream_world.front_default,
+            types: data.types.map(typeInfo => typeInfo.type.name)
+        };
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-};
+}
 
-
-function showPokemons() {
+async function showPokemons() {
     pokemon.forEach(async (item) => {
         const div = document.createElement("div");
         div.classList.add("pokemons");
 
         const img = document.createElement("img");
         img.alt = item.name;
-        img.src = await fetchPokemonData(item.url);
 
         const name = document.createElement("h1");
         name.textContent = item.name;
 
-        div.append(img, name);
+        const { image, types } = await fetchPokemonData(item.url);
+        img.src = image;
+
+        const typePara = document.createElement("p");
+        typePara.textContent = ` ${types.join(", ")}`;
+
+
+        types.forEach(type => {
+            const option = document.createElement("option");
+            option.value = type;
+            option.innerHTML = type;
+            typeFilter.appendChild(option);
+        });
+
+        div.append(img, name, typePara);
         pokemonsShowBox.append(div);
     });
 }
 
+
 showPokemons();
+
+
+function filterPokemonsByTypes () {
+    
+}
